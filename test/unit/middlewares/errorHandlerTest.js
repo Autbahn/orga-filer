0353cfe2674f07ffaa4ac31a18f9ede17e57a07c
Jan Lumbeck <ja.lumbeck@gmail.com>
2015-06-18 12:16:29 +0200

    Catching thrown errors

--- test/unit/middlewares/errorHandlerTest.js
+++ test/unit/middlewares/errorHandlerTest.js
@@ -2,19 +2,40 @@ var request = require('supertest');
 var app = require('express')();
 var errorHandler = require('../../..//middlewares/errorHandler');
 var should = require('should');
+var hasThrown = false;
+
+beforeEach(function() {
+    hasThrown = false; 
+});
+app.use('/successful', function(req, res) {
+   res.send('Request successful!'); 
+});
 
 app.use(errorHandler.err404);
+app.use(function(err, req, res, next) {
+    hasThrown = true;
+    next();
+});
+
 describe("errorHandlerTest", function() {
 
-    describe("When response is 404", function() {
+    describe("When the request is not successful", function() {
 
-        it("it should add the custom error message", function(done) {
-                request(app).get('/').end(function (err, res) {
-                    res.error.text.should.startWith("Error: Not Found<br> &nbsp; &nbsp;at");
+        it("it should throw the error", function(done) {
+                request(app).get('/shouldfail').end(function (err, res) {
+                    hasThrown.should.be.true;
                     done();
                 });
         });
-
+    });
+    
+    describe("When the request is successful", function() {
+       it("it should not throw the error", function(done) {
+           request(app).get('/successful').end(function (err, res) {
+                hasThrown.should.be.false;
+                done();
+            });
+       }) 
     });
 
 });
