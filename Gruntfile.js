30b3c4eb2a5f7955e41e2041b2efdccbe3bfed66
Jan Lumbeck <ja.lumbeck@gmail.com>
2015-06-22 18:46:23 +0200

    Adding coveralls.io

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,6 +64,24 @@ module.exports = function (grunt) {
             }
         },
 
+        coveralls: {
+            // Options relevant to all targets
+            options: {
+                // When true, grunt-coveralls will only print a warning rather than
+                // an error, to prevent CI builds from failing unnecessarily (e.g. if
+                // coveralls.io is down). Optional, defaults to false.
+                force: false
+            },
+
+            all: {
+                // LCOV coverage file (can be string, glob or array)
+                src: 'test/coverage/reports/lcov.info',
+                options: {
+                    // Any options for just this target
+                }
+            }
+        },
+
         env: {
             coverage: {
                 APP_DIR_FOR_CODE_COVERAGE: './test/coverage/instrument/app/'
@@ -120,6 +138,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-istanbul');
     grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-coveralls');
 
     grunt.registerTask('development', ['jshint', 'simplemocha:all']);
 
